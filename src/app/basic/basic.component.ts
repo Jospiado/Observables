b6e0b020fc689b06312bc511e9088242ab1cdf7d
Jospiado <bugsescripts@hotmail.com>
2022-05-16 09:57:10 -0300

    Second Commit

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-basic',
@@ -11,5 +12,23 @@ export class BasicComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  const myFirstObservable = new Observable(
+    (observer: Observer<number>) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.next(4);
+      observer.next(5);
+      observer.complete();
+    }
+  );
+    myFirstObservable.subscribe((n: number) => console.log(n),
+  (error) => console.error(error),
+  () => console.log('Completed.');
 
+const timerCount = interval(500);
+timerCount.subscribe(
+  (n) => console.log(n)
+)
+console.log("After interval");
 }
